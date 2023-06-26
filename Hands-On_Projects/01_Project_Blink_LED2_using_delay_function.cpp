7a15acb7e599cc0ad68a557789b55d6ffc14f15b
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-06-26 15:08:10 +0530

    Update 01_Project_Blink_LED2_using_delay_function.cpp

--- Hands-On_Projects/01_Project_Blink_LED2_using_delay_function.cpp
+++ Hands-On_Projects/01_Project_Blink_LED2_using_delay_function.cpp
@@ -6,14 +6,16 @@
 
 #include "mbed.h"
 
-DigitalOut ledPin(D13);
+DigitalOut ledPin(D13);  // Define DigitalOut object for the LED pin (D13)
+
 int main()
 {   
     while (true) 
     {
-        ledPin.write(1);
-        HAL_Delay(1000);
-        ledPin.write(0);
-        HAL_Delay(10000);
+        ledPin.write(1);  // Set the LED pin high, turning on the LED
+        HAL_Delay(1000);  // Delay for 1 second (1000 milliseconds)
+
+        ledPin.write(0);  // Set the LED pin low, turning off the LED
+        HAL_Delay(10000);  // Delay for 10 seconds (10000 milliseconds)
     }
 }
