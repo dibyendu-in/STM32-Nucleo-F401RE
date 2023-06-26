11414b340dc3f61c5e2cc21307ec50ffd9511dcd
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-06-26 14:43:52 +0530

    Create 01_Project_Blink_LED2_using_delay_function.cpp

---
+++ Hands-On_Projects/01_Project_Blink_LED2_using_delay_function.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: DIBYENDU BARMAN
+ * Date: 26-June-2023
+ * Project Description: Blink LED2 using delay function
+ */
+
+#include "mbed.h"
+
+DigitalOut ledPin(D13);
+int main()
+{   
+
+    while (true) 
+    {
+        ledPin.write(1);
+        HAL_Delay(1000);
+        ledPin.write(0);
+        HAL_Delay(10000);
+    }
+}
