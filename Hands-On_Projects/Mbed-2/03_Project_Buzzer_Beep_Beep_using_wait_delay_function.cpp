45ee64cc229df21008c08643c5bff81af535d5e6
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-07-02 22:48:06 +0530

    Create 03_Project_Buzzer_Beep_Beep_using_wait_delay_function.cpp

---
+++ Hands-On_Projects/Mbed-2/03_Project_Buzzer_Beep_Beep_using_wait_delay_function.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: DIBYENDU BARMAN
+ * Date: 02-July-2023
+ * Project Description: Buzzer Beep Beep @D2 using wait delay function
+*/
+
+#include "mbed.h"
+
+DigitalOut buz(D2);  // Define DigitalOut object for the D2 pin (D2)
+
+int main()
+{   
+    while (true) 
+    {
+        buz.write(1);  // Set the buzzer pin high, turning on the buzzer
+        wait_ms(300);  // Delay for 300 milliseconds
+
+        buz.write(0);  // Set the LED low, turning off the LED
+        wait_ms(300);  // Delay for 300 milliseconds
+    }
+}
