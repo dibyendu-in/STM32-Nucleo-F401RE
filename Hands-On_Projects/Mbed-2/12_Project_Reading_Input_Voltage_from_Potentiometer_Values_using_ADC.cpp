0a5fbb4f06a1477bd9bf3a2cdae8dc67b8c979c4
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-07-04 11:21:12 +0530

    Create 12_Project_Reading_Input_Voltage_from_Potentiometer_Values_using_ADC.cpp

---
+++ Hands-On_Projects/Mbed-2/12_Project_Reading_Input_Voltage_from_Potentiometer_Values_using_ADC.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author: DIBYENDU BARMAN
+ * Date: 04-July-2023
+ * Project Description: Reading Input Voltage from Potentiometer Values using ADC
+ */
+
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX);   // Serial object for PC communication
+AnalogIn POT(A0);          // Analog input for potentiometer
+
+int main()
+{
+    while (true) 
+    {
+        float ADC_val = POT.read();                     // Read potentiometer value
+        printf("ADC val: %f \n", ADC_val);              // Print potentiometer value
+        float ADC_volts = ADC_val * 3.3;                // Convert potentiometer value to voltage
+        printf("ADC voltage: %f \n", ADC_volts);        // Print potentiometer voltage
+        wait_ms(500);                                   // Delay for 500 milliseconds
+    }
+}
